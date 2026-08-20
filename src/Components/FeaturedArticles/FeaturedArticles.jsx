aa67cfea348d4defd2ff774a43081c0b724da48d
rashelrana <rashelrana2678@gmail.com>
2026-08-21 01:31:46 +0600

    done to make one route

--- src/Components/FeaturedArticles/FeaturedArticles.jsx
+++ src/Components/FeaturedArticles/FeaturedArticles.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getToLocalStorage } from '../../utilities/localStorage'
+import { Link, Navigate, useLoaderData, useNavigate } from 'react-router';
+import Book from '../Book/Book';
 
 function FeaturedArticles() {
+    const [readBooksCount, setReadBooksCount] = useState(0);
+    const [filterBooks, setFilterBooks] = useState([]);
+    const books = useLoaderData();
+    const navigate = useNavigate();
+    useEffect(() => {
+        const readBooks = getToLocalStorage('read');
+        const readIds =readBooks.map(String);
+        const filteredBooks = books.filter(book => !readIds.includes(String(book.bookId)))
+        setFilterBooks(filteredBooks)
+        setReadBooksCount(readBooks.length)
+    },[])
+    const goalBooks = 20;
+    const percentage = Math.min(
+        Math.round((readBooksCount/goalBooks)* 100),
+        100
+    )
+    
+    const handleBookDetails = (id) =>{
+        navigate(`/bookDetails/${id}`);
+    }
   return (
-    <div>
-        
+   <div>
+        <div className="bg-base-200 p-6 rounded-2xl max-w-xl mx-auto my-8 text-center shadow-md">
+            <h3 className="text-xl font-bold mb-2">🎯 2026 Reading Challenge</h3>
+            <p className="text-gray-600 mb-4">You have read {readBooksCount} out of {goalBooks} books this year!</p>
+            
+            {/* DaisyUI Progress Bar */}
+            <progress className="progress progress-success w-full" value={percentage} max="100"></progress>
+            <span className="text-sm text-gray-500 mt-2 block">You Almost completed {percentage}%</span>
+                
+        </div>
+        <div className='mb-10'>
+            <h2 className='font-black text-3xl my-20 text-center'>More Suggested Books</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 cursor-pointer'>
+                {
+                    filterBooks.map(book => <Book book={book} handleBookDetails={handleBookDetails} ></Book>)
+                }
+            </div>
+        </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -40,7 +40,8 @@ const router = createBrowserRouter([
       },
       {
         path: 'featuredArticles',
-        element: <FeaturedArticles></FeaturedArticles>
+        element: <FeaturedArticles></FeaturedArticles>,
+        loader: () => fetch('/book.json')
       },
       {
         path: 'reviews',
